2ef0d2fe3314976080f8be5be39c2619ead5ff9e
avalentova <andy.valentova@gmail.com>
2022-03-27 14:07:05 +0200

    Úkol č. 3 - SMS brána

--- ukol_03_sms_brana.py
+++ ukol_03_sms_brana.py
@@ -1,4 +1,24 @@
-telefonni_cislo = input("Zadej telefonní číslo příjemce: ")
+# telefonni_cislo = input("Zadej telefonní číslo příjemce: ")
+
+# def delka(telefonni_cislo):
+#     if len(telefonni_cislo) == 9 or len(telefonni_cislo) == 13:
+#         return True
+#     else:
+#         return False
+
+# if delka(telefonni_cislo) == True:
+#     text = input ("Zadej text zprávy: ")
+#     def cena(text):
+#         if len(text)//180 < 1:
+#             return 1 * 3
+#         else:
+#             return (len(text)//180 + 1) * 3
+#     print(f"Výsledná cena zprávy je {cena(text)} Kč")
+# else:
+#     print ("Neplatné telefonní číslo.")
+
+# upravena verze kodu
+import math
 
 def delka(telefonni_cislo):
     if len(telefonni_cislo) == 9 or len(telefonni_cislo) == 13:
@@ -6,15 +26,14 @@ def delka(telefonni_cislo):
     else:
         return False
 
+def cena(text):
+    vypocet = math.ceil(len(text)/180) * 3
+    print(f"Výsledná cena zprávy je {vypocet} Kč")
+
+telefonni_cislo = input("Zadej telefonní číslo příjemce: ")
+
 if delka(telefonni_cislo) == True:
     text = input ("Zadej text zprávy: ")
-    def cena(text):
-        if len(text)//180 < 1:
-            return 1 * 3
-        else:
-            return (len(text)//180 + 1) * 3
-    print(f"Výsledná cena zprávy je {cena(text)} Kč")
+    cena(text)
 else:
     print ("Neplatné telefonní číslo.")
-
-
